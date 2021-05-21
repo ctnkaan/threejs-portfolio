1287553bb8a51c4c2d7a881a396436d040516442
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-05-21 21:09:18 +0300

    setup v2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector("#bg"),
 })
 
